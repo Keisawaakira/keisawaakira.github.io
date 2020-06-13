dce658534f20b706189ea78f91417fa8eddfcdde
Keisawaakira <54810442+Keisawaakira@users.noreply.github.com>
2020-06-13 19:51:05 +0900

    Update music.js

--- assets/music.js
+++ assets/music.js
@@ -5,21 +5,21 @@ $(document).one('click', function(event){
 	  listMaxHeight: 176,
 	  autoplay: true,
       audio: [{
+	  name: '花一匆',
+          artist: 'Unknown',
+          url: '/assets/music/花一匆.mp3',
+          cover: '/assets/music/21世紀を手に入れろ.jpg'
+	  },
+	  {
           name: '21世紀を手に入れろ',
           artist: 'クレヨンしんちゃん 嵐を呼ぶ モーレツ! サウンドトラック',
           url: '/assets/music/21世紀を手に入れろ.mp3',
           cover: '/assets/music/21世紀を手に入れろ.jpg'
-      },
+          },
 	  {
 		  name: 'saving me',
           artist: 'Unknown',
           url: '/assets/music/saving me.aac',
-          cover: '/assets/music/21世紀を手に入れろ.jpg'
-	  },
-	  {
-		  name: '花一匆',
-          artist: 'Unknown',
-          url: '/assets/music/花一匆.mp3',
           cover: '/assets/music/21世紀を手に入れろ.jpg'
 	  }
 	  ]
